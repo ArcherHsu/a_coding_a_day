6371d595892f7f6a17375fdab481e46ff52b8746
Archer <a0983770181@gmail.com>
2023-11-23 17:22:32 +0800

    user can add new issue

--- n_p.py
+++ n_p.py
@@ -1,4 +1,9 @@
 arr = []
+with open('n_p.csv', 'r') as f:
+	for line in f:
+		s=line.strip().split(',')
+		arr.append(s)
+		
 while True:
 	name = input('請輸入商品名稱')
 	if name=='Q' or name=='q':
@@ -8,9 +13,9 @@ while True:
 		break	
 	n_p = [name, price]
 	arr.append(n_p)
-print(arr)
 
 with open('n_p.csv','w') as f:
-	f.write('商品,價格\n')
+	if arr[0] !=['商品', '價格']:
+		f.write('商品,價格\n')
 	for i in arr:
 		f.write(str(i[0])+','+str(i[1])+'\n')
