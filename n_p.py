76af52448899f0d9741e292eb7d572daf7126eaa
Archer <a0983770181@gmail.com>
2023-11-24 21:28:26 +0800

    make watch easy

--- n_p.py
+++ n_p.py
@@ -1,9 +1,15 @@
+#讀取檔案
 arr = []
-with open('n_p.csv', 'r') as f:
+with open('n_p2.csv', 'r') as f:
 	for line in f:
+		if '商品,價格' in line:
+			continue
 		s=line.strip().split(',')
 		arr.append(s)
-		
+print(arr)
+
+
+#使用者輸入
 while True:
 	name = input('請輸入商品名稱')
 	if name=='Q' or name=='q':
@@ -14,8 +20,10 @@ while True:
 	n_p = [name, price]
 	arr.append(n_p)
 
-with open('n_p.csv','w') as f:
-	if arr[0] !=['商品', '價格']:
-		f.write('商品,價格\n')
+
+#存檔
+with open('n_p2.csv','w') as f:
+	f.write('商品,價格\n')
 	for i in arr:
 		f.write(str(i[0])+','+str(i[1])+'\n')
+print(arr)		
